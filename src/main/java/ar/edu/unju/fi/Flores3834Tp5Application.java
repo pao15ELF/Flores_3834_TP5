d0a174c84d3723df34a3858a7d4a933d2f3f5ffa
Paola <Paola@DESKTOP-U6UR9H7>
2020-06-01 02:09:14 -0300

    Asunto: Inicialización del proyecto. Responsable: Flores Paola Fernanda. Task00:Inicializando el proyecto de Sprin Boot del Trabajo Practico N°5.

---
+++ src/main/java/ar/edu/unju/fi/Flores3834Tp5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Flores3834Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Flores3834Tp5Application.class, args);
+	}
+
+}
